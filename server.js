f4e08dd1f3425a1f2355a65a2b5997b2a3692a1a
Justin Bridwell <jcbridwe@gmail.com>
2018-05-21 02:28:58 +0000

    Updates to server.js, index.html, and custom.css to clean up code

--- server.js
+++ server.js
@@ -80,5 +80,5 @@ function broadcast(event, data) {
 
 server.listen(process.env.PORT || 3000, process.env.IP || "0.0.0.0", function(){
   var addr = server.address();
-  console.log("Chat server listening at", addr.address + ":" + addr.port);
+  console.log("Server listening at", addr.address + ":" + addr.port);
 });
